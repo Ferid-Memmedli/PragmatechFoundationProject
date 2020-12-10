cbe672b446bb5dfa0b007ba6ada2647da389209a
Ferid Memmedli <shahasist@gmail.com>
2020-12-10 22:19:27 +0400

    update

--- jscript/script.js
+++ jscript/script.js
@@ -1,11 +1,11 @@
 /* let a=50;
 document.querySelector(".btn").addEventListener('click',function(){
-    let box=document.createElement("div")
-    box.className="box"
-    a++
-    box.innerHTML=a
-    let container=document.querySelector('.container')
-    container.appendChild(box)
+    // let box=document.createElement("div")
+    // box.className="box"
+    // a++
+    // box.innerHTML=a
+    // let container=document.querySelector('.container')
+    // container.appendChild(box)
 })
     a=a+100
     let box=document.querySelector(".box");
