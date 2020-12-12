c58c78cac29ff679abad9b71c21d82f1f8d5a28f
Ferid Memmedli <shahasist@gmail.com>
2020-12-12 13:06:56 +0400

    update

--- jscript/script.js
+++ jscript/script.js
@@ -10,25 +10,6 @@ document.querySelector(".btn").addEventListener('click',function(){
     a=a+100
     let box=document.querySelector(".box");
     box.style.left=a+"px";
------------------------------------------------------------------
-function ClickEvent(){
-    console.log("Hello World")
-}
-let nextBtn=document.querySelector(".btn-next");
-let prevBtn=document.querySelector(".btn-prev");
-let slider=document.querySelector('.slider-items');
-a=1;
-nextBtn.addEventListener("click",function(){
-
-    a+=600;
-    slider.style.left=a+'px';
-})
-
-prevBtn.addEventListener("click",function(){
-
-    a-=600;
-    slider.style.left=a+'px';
-})
 ------------------------------------------------------
 x=1
 setInterval(function(){
