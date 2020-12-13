ee66a2a4d10e4fc405ca4c93ca1b6994fd655136
Ferid Memmedli <shahasist@gmail.com>
2020-12-13 04:03:18 +0400

    header nav hisesinde JS islendi

--- jscript/script.js
+++ jscript/script.js
@@ -23,8 +23,51 @@ div.style.top=randY+'px'
 document.body.appendChild(div)
 },100)*/
 
+// -----------------------------------------Header nav----------------------------------------
+let nav = document.querySelector(".nav");
+let a = document.querySelectorAll(".blank");
+
+
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        nav.style.transition = 'all 0.5s ease-in-out';
+        nav.style.background = "white";
+        nav.style.boxShadow = "0 2px 8px 3px rgba(0, 0, 0, 0.050980392156862744)";
+        a.forEach(i => {
+            i.addEventListener('mouseover', function() {
+                i.style.color = "#e65f78";
+            });
+            i.addEventListener('mouseout', function() {
+                i.style.color = "black";
+            });
+        });
+        for (i in a) {
+            a[i].style.color = "black";
+        }
+    } else {
+        nav.style.transition = 'all 0.5s ease-in-out';
+        nav.style.background = "none";
+        nav.style.boxShadow = "none";
+        a.forEach(i => {
+            i.addEventListener('mouseover', function() {
+                i.style.color = "#e65f78";
+            });
+            i.addEventListener('mouseout', function() {
+                i.style.color = "white";
+            });
+        }); 
+        for (i in a) {
+            a[i].style.color = "white"
+        }
+    }
+}
+
 // ------------------------------- Our client bolmesi ucun yazilmis JS kodlari
-var dots = document.querySelectorAll('.aktiv1')
+let dots = document.querySelectorAll('.aktiv1')
 dots.forEach(i => {
     i.addEventListener('click', e => {
         dots.forEach(i => i.classList.remove('active'))
@@ -32,10 +75,10 @@ dots.forEach(i => {
     })
 })
 
-var akt1 = document.querySelector('#active1');
-var akt2 = document.querySelector('#active2');
-var akt3 = document.querySelector('#active3');
-var ata = document.querySelector('#ata');
+let akt1 = document.querySelector('#active1');
+let akt2 = document.querySelector('#active2');
+let akt3 = document.querySelector('#active3');
+let ata = document.querySelector('#ata');
 
 akt1.addEventListener('click', function () {
     ata.style.left = '0px';
