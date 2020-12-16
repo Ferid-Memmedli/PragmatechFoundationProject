8600f7274eec9bb1f883d09a73c8c651bdd36908
Ferid Memmedli <shahasist@gmail.com>
2020-12-16 12:52:30 +0400

    scrol duzelisler edildi

--- jscript/script.js
+++ jscript/script.js
@@ -62,47 +62,38 @@ akt3.onclick = function () {
 };
 
 //----------------------Problem-----------------
-// let sections = document.querySelectorAll("section");
-// let blanks = document.querySelectorAll(".blank");
-// for (let i = 0; i < sections.length; i++) {
-//     for (let b = 0; b < blanks.length; b++) {
-//         if (scrollY > sections[i].offsetTop) {
-//             blanks[b].classList("isik");
-//         }
-//     }
-// }
 function scrl() {
     let header = document.getElementById("header");
     if  (scrollY > header.offsetTop) {
         blanks[0].classList.add("isik");
         blanks[1].classList.remove("isik");
     }
-    if (scrollY > sections[0].offsetTop) {
+    if (scrollY > sections[0].offsetTop - 40) {
         blanks[0].classList.remove("isik");
         blanks[1].classList.add("isik");
         blanks[2].classList.remove("isik");
     }
-    if (scrollY > sections[1].offsetTop) {
+    if (scrollY > sections[1].offsetTop - 40) {
         blanks[1].classList.remove("isik");
         blanks[2].classList.add("isik");
         blanks[3].classList.remove("isik");
     }
-    if (scrollY > sections[2].offsetTop) {
+    if (scrollY > sections[2].offsetTop - 40) {
         blanks[2].classList.remove("isik");
         blanks[3].classList.add("isik");
         blanks[4].classList.remove("isik");
     }
-    if (scrollY > sections[3].offsetTop) {
+    if (scrollY > sections[3].offsetTop - 40) {
         blanks[3].classList.remove("isik");
         blanks[4].classList.add("isik");
         blanks[5].classList.remove("isik");
     }
-    if (scrollY > sections[4].offsetTop) {
+    if (scrollY > sections[4].offsetTop - 40) {
         blanks[4].classList.remove("isik");
         blanks[5].classList.add("isik");
         blanks[6].classList.remove("isik");
     }
-    if (scrollY > sections[5].offsetTop) {
+    if (scrollY > sections[5].offsetTop - 40) {
         blanks[5].classList.remove("isik");
         blanks[6].classList.add("isik");
     }
