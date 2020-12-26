76684586d7ac35a1dd630dbc0f1c8591dda76817
Ferid Memmedli <shahasist@gmail.com>
2020-12-26 23:25:30 +0400

    update

--- blog/jscript/script.js
+++ blog/jscript/script.js
@@ -1,5 +1,4 @@
 let nav = document.querySelector(".nav");
-let mybutton = document.getElementById("myBtn");
 let blanks = document.querySelectorAll(".blank");
 let logo = document.getElementById("darklogo");
 let sections = document.querySelectorAll("section");
@@ -8,7 +7,6 @@ let header = document.getElementById("header");
 // -----------------------------------------Header nav----------------------------------------
 window.onscroll = function () {
     scrollFunction();
-    scrl();
 };
 
 function scrollFunction() {
@@ -18,7 +16,6 @@ function scrollFunction() {
         nav.style.padding = ('15px 0px');
         nav.style.background = "white";
         nav.style.boxShadow = "0 2px 8px 3px rgba(0, 0, 0, 0.05)";
-        mybutton.style.display = "block";
         blanks.forEach(i => {
             i.style.color = "black";
         });
@@ -28,54 +25,8 @@ function scrollFunction() {
         nav.style.padding = ('20px 0px');
         nav.style.background = "none";
         nav.style.boxShadow = "none";
-        mybutton.style.display = "none";
         blanks.forEach(i => {
             i.style.color = "white";
         });
     };
 };
-// -------------------------------button top---------------------------------
-mybutton.onclick = function () {
-    var a = document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-};
-
-//----------------------Scroll menu--------------------
-function scrl() {
-    blanks.forEach(i => i.classList.remove('isik'));
-    if (scrollY > sections[5].offsetTop - 45) {
-        blanks[6].classList.add("isik");
-    } else if (scrollY > sections[4].offsetTop - 45) {
-        blanks[5].classList.add("isik");
-    } else if (scrollY > sections[3].offsetTop - 45) {
-        blanks[4].classList.add("isik");
-    } else if (scrollY > sections[2].offsetTop - 45) {
-        blanks[3].classList.add("isik");
-    } else if (scrollY > sections[1].offsetTop - 45) {
-        blanks[2].classList.add("isik");
-    } else if (scrollY > sections[0].offsetTop - 45) {
-        blanks[1].classList.add("isik");
-    } else {
-        blanks[0].classList.add("isik");
-    }
-};
-
-// ---------------------------Formdan melumat goturmeq-------------------------------------
-// const form = document.forms[0];
-
-// form.addEventListener("submit", function (event) {
-//     event.preventDefault();
-//     const { name, email, subject, comments } = this.elements;
-//     console.log(name.value, email.value, subject.value, comments.value);
-// });
-
-const form = document.forms[0];
-
-form.addEventListener("submit", function (event) {
-    event.preventDefault();
-    const formData = new FormData(this);
-
-    for (const formElement of formData) {
-        console.log(formElement);
-    }
-});
