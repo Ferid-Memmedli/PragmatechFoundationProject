841576b5f66e03611d2c738f44279e44de45843e
Ferid Memmedli <shahasist@gmail.com>
2020-12-27 23:06:13 +0400

    update

--- blog/jscript/script.js
+++ blog/jscript/script.js
@@ -11,13 +11,13 @@ function scrollFunction() {
         nav.style.transition = 'all 0.5s ease-in-out';
         nav.style.padding = ('15px 0px');
         nav.style.background = "white";
-        nav.style.boxShadow = "0 2px 8px 3px rgba(0, 0, 0, 0.05)";
+        // nav.style.boxShadow = "0 2px 8px 3px rgba(0, 0, 0, 0.05)";
 
     } else {
         nav.style.transition = 'all 0.5s ease-in-out';
         nav.style.padding = ('20px 0px');
         nav.style.background = "none";
-        nav.style.boxShadow = "none";
+        // nav.style.boxShadow = "none";
 
     };
 };
