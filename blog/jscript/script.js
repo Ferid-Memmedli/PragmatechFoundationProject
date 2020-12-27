c3fb1689ca9848e635a93d53c00bc0afdf2b2324
Ferid Memmedli <shahasist@gmail.com>
2020-12-27 20:47:48 +0400

    update

--- blog/jscript/script.js
+++ blog/jscript/script.js
@@ -1,6 +1,5 @@
 let nav = document.querySelector(".nav");
 let blanks = document.querySelectorAll(".blank");
-let logo = document.getElementById("darklogo");
 
 // -----------------------------------------Header nav----------------------------------------
 window.onscroll = function () {
@@ -9,22 +8,16 @@ window.onscroll = function () {
 
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        logo.setAttribute("src", "img/logo-dark.png");
         nav.style.transition = 'all 0.5s ease-in-out';
         nav.style.padding = ('15px 0px');
         nav.style.background = "white";
         nav.style.boxShadow = "0 2px 8px 3px rgba(0, 0, 0, 0.05)";
-        blanks.forEach(i => {
-            i.style.color = "black";
-        });
+
     } else {
-        logo.setAttribute("src", "img/logo.png");
         nav.style.transition = 'all 0.5s ease-in-out';
         nav.style.padding = ('20px 0px');
         nav.style.background = "none";
         nav.style.boxShadow = "none";
-        blanks.forEach(i => {
-            i.style.color = "white";
-        });
+
     };
 };
