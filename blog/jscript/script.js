a1e755dd5ca2558a992e26ce72696ea1834a4538
Ferid Memmedli <shahasist@gmail.com>
2021-01-04 15:44:04 +0400

    header animasiya  hissesi elave olundu

--- blog/jscript/script.js
+++ blog/jscript/script.js
@@ -1,5 +1,7 @@
 let nav = document.querySelector(".nav");
-let blanks = document.querySelectorAll(".blank");
+let blank = document.querySelector(".blank");
+let logo = document.getElementById("darklogo");
+let header = document.getElementById("header");
 
 // -----------------------------------------Header nav----------------------------------------
 window.onscroll = function () {
@@ -8,16 +10,32 @@ window.onscroll = function () {
 
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        logo.setAttribute("src", "img/logo-dark.png");
         nav.style.transition = 'all 0.5s ease-in-out';
         nav.style.padding = ('15px 0px');
         nav.style.background = "white";
-        // nav.style.boxShadow = "0 2px 8px 3px rgba(0, 0, 0, 0.05)";
-
+        nav.style.boxShadow = "0 2px 8px 3px rgba(0, 0, 0, 0.05)";
+        blank.style.color = "black";
     } else {
+        logo.setAttribute("src", "img/logo.png");
         nav.style.transition = 'all 0.5s ease-in-out';
         nav.style.padding = ('20px 0px');
         nav.style.background = "none";
-        // nav.style.boxShadow = "none";
-
+        nav.style.boxShadow = "none";
+        blank.style.color = "white";
     };
 };
+//--------------------------------------------
+
+document.addEventListener('DOMContentLoaded', function () {
+    var typed = new Typed('#typed', {
+        stringsElement: '#typed-strings',
+        typeSpeed: 200,
+        backSpeed: 100,
+        startDelay: 1000,
+        cursorChar: '|',
+        loop: true,
+        fadeOut: false,
+        loopCount: Infinity,
+    });
+});
